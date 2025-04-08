6653686ac1962592ec65539210541be65efc454b
Ryan <ryanenslow@gmail.com>
2025-04-08 14:51:40 -0600

    Simplified redundant code and added comments for clarity

--- SeniorProject.cpp
+++ SeniorProject.cpp
@@ -1,21 +1,26 @@
+/*==========================================================
+; Program Name: SeniorProject.cpp
+;
+; Authors: Ryan Enslow & Garrett Nadauld
+;
+; Description:
+; This program provides an auto-leveling system for the RC 2 Ellipsometer
+; made by J.A. Woolam. Used in in-situ temperature testing this program
+; utilizes a lateral effect sensor and laser to track the deformation of
+; the sample being tested. Two Stepper motors X and Y use the data from
+; the sensor to adjust the test bed accordingly.
+;
+; Company: Weber State University
+;
+;========================================================== */
+
 #include "ClearCore.h"
 
-// Specify which motors to move.
+// Stepper motor set up:
 // Options are: ConnectorM0, ConnectorM1, ConnectorM2, or ConnectorM3.
-#define motorX ConnectorM0
-#define motorY ConnectorM1
 
-// Defines the bit-depth of the ADC readings (8-bit, 10-bit, or 12-bit)
-// Supported adcResolution values are 8, 10, and 12
-#define adcResolution 12
-
-// Select the baud rate to match the target serial device
-#define baudRate 9600
- 
-// Specify which serial connector to use: ConnectorUsb, ConnectorCOM0, or
-// ConnectorCOM1
-#define SerialPort ConnectorUsb
- 
+#define motorX ConnectorM0 //motor X is connected to M0 on the clear core
+#define motorY ConnectorM1 //motor Y is connected to M1 on the clear core
 // This example has built-in functionality to automatically clear motor alerts, 
 //  including motor shutdowns. Any uncleared alert will cancel and disallow motion.
 // WARNING: enabling automatic alert handling will clear alerts immediately when 
@@ -29,8 +34,12 @@
 // Define the velocity and acceleration limits to be used for each move
 const int32_t velocityLimit = 10000; // 10000pulses per sec
 const int32_t accelerationLimit = 10000; //50000 pulses per sec^2
- 
 
+
+//ADC Set up:
+// Defines the bit-depth of the ADC readings (8-bit, 10-bit, or 12-bit)
+// Supported adcResolution values are 8, 10, and 12
+#define adcResolution 12
 
 int16_t leveling; // State of input switch
 
@@ -46,9 +55,9 @@ void HandleAlertsX();
  * Main
  *
  *    Main loop, reads analog input of SUM, deltaX, deltaY
- *		 checks if laser position is within x and y tolerance
- *               calls move motor if out of tolerance, alternates starting with x and y
- *               loops every 1 second 
+ *	  checks if laser position is within x and y tolerance
+ *    calls move motor if out of tolerance, alternates starting with x and y
+ *    adjusts every 0.75 second.
  *    
  *
  * Parameters:
@@ -56,7 +65,7 @@ void HandleAlertsX();
  *
  * Returns: 
  *    None
- */
+ -----------------------------------------------------------------------------*/
  int main() {
 
     // Set the resolution of the ADC.
@@ -78,31 +87,24 @@ void HandleAlertsX();
 	motorY.EnableRequest(false);
 	
 
-    double inputVoltageSUM, inputVoltageY, inputVoltageX, deltaX, deltaY, Xtol, Ytol, SumX, SumY, SumSum, voltageX, voltageY, voltageSum = 0.0;
+    double inputVoltageSUM, inputVoltageY, inputVoltageX, SumX, SumY, SumSum, voltageX, voltageY, voltageSum = 0.0; //tracks voltages
     bool LevelFlag, ledState = false;	//Used to set level position of first iteration of loop
-    //bool Xflag = true;	//Used to determine which axis to move first
-    double LevelX, LevelY, Xpos, Ypos = 0.0;
-	int count = 0;
-	//Define constants for PDP90A
-	//const double Lx = 10E-3; // Length of x axis in m
-	//const double Ly = 10E-3; // Length of y axis in m
-	//const double en = 300E-6; // Output noise voltage in Vrms
-	const double Xvoltol = 2E-2; // X axis tolerance
-	const double Yvoltol = 2E-2; // Y axis tolerance
-	const double deltavoltolY = 2E-4; // Steps for smallest delta voltage
-	const double deltavoltolX = 2E-4; // Steps for smallest delta voltage
+    double LevelX, LevelY, Xpos, Ypos = 0.0; //used to track the desired voltages when leveling is activated
+	int count = 0; //takes 10 samples then computes the average
+	const double Xtol = 2E-2; // X axis tolerance
+	const double Ytol = 2E-2; // Y axis tolerance
+	const double deltaY = 2E-4; // Steps for smallest delta voltage
+	const double deltaX = 2E-4; // Steps for smallest delta voltage
 	
-    int16_t adcSUM, adcY, adcX = 0;
+    int16_t adcSUM, adcY, adcX = 0; //adc read values
  
     while (true) {
-	
 		
-        //inputVoltageSUM = 0.0;
-        //inputVoltageY = 0.0;
-        //inputVoltageX = 0.0;
-		leveling = inputPin.State();
-        // Read the analog input (A-9 through A-12 may be configured as analog
-        // inputs).
+		//update the leveling switch state
+		leveling = inputPin.State(); 
+		
+        // Read the analog input (A-9 through A-12 may be configured as analog inputs).
+		
         adcSUM = ConnectorA12.State();
         // Convert the reading to a voltage.
         voltageSum = 10.0 * adcSUM / ((1 << adcResolution) - 1);
@@ -115,12 +117,14 @@ void HandleAlertsX();
         // Convert the reading to a voltage.
         voltageX = 10.0 * adcX / ((1 << adcResolution) - 1);
 		
+		//Collect 10 voltage samples for Sum, X, and Y
 		SumX += voltageX;
 		SumY += voltageY;
 		SumSum += voltageSum;
 		
 		if(count == 10)
 		{
+			//Compute the average for each voltage and set sum back to zero
 			inputVoltageX = SumX/10;
 			inputVoltageY = SumY/10;
 			inputVoltageSUM =  SumSum/10;
@@ -132,12 +136,14 @@ void HandleAlertsX();
 			if (leveling) 
 			{	//Once switch has been set to the on position the bed is level, enter automated leveling state
 				
+				//enable motors when leveling, this will disable manual adjustments and turn on motors.
 				motorX.EnableRequest(true);
 				motorY.EnableRequest(true);
+				
 				Xpos = inputVoltageX;	//New laser position for X
 				Ypos = inputVoltageY;	//New laser position for Y
 				
-				if(inputVoltageSUM < 2.5)
+				if(inputVoltageSUM < 2.5) //Check if laser is still on the sensor, if not don't adjust and blink connector LED
 				{
 					ConnectorLed.State(ledState);
 					ledState = !ledState;
@@ -145,58 +151,40 @@ void HandleAlertsX();
 				
 				
 				else if (LevelFlag==false) 
-				{	//If level flag is false calibrate level position to be compared to new level data and set flag to true
+				{	//If level flag is false create level position for reference to new level data and set flag to true
 					LevelX = inputVoltageX;	//Set LevelX sensor position
 					LevelY = inputVoltageY;	//Set LevelY sensor position
-					Xtol = Xvoltol;
-					Ytol = Yvoltol;
-					deltaX = deltavoltolX;
-					deltaY = deltavoltolY;
-					LevelFlag = true;
+					LevelFlag = true; //set flag to true as to not rewrite the leveled voltages
 				}
 
 				else 
-				{ // If Xflag is true move X axis first
-					
-                
-					
+				{
+					//Make X and Y adjustments if new x or y position is not within tolerance of the leveled values
 					
 					if ((Xpos > (LevelX + Xtol)) || (Xpos < (LevelX - Xtol))) 
-					{ // If Xpos is greater than LevelX + Xtol
+					{ // If Xpos is greater than LevelX + Xtol or less than LevelX - Xtol
 						MoveDistanceX(int32_t((LevelX - Xpos) / deltaX));
 					} 
 					
 					if ((Ypos > (LevelY + Ytol))|| Ypos < (LevelY - Ytol)) 
-					{ // If Ypos is greater than LevelY + Ytol
+					{ // If Ypos is greater than LevelY + Ytol or less than LevelY - Ytol
 						MoveDistanceY(int32_t((Ypos - LevelY) / deltaY));
 					} 
 				
 				} 
-				
-	/*			else 
-				{ // If Xflag is false move Y axis first
-					if ((Ypos > (LevelY + Ytol))|| Ypos < (LevelY - Ytol)) 
-					{ // If Ypos is greater than LevelY + Ytol
-					   MoveDistanceY( int32_t((LevelY - Ypos) / delta));
-					}   
-				
-					if ((Xpos > (LevelX + Xtol)) || (Xpos < (LevelX - Xtol))) 
-					{ // If Xpos is greater than LevelX + Xtol
-						MoveDistanceX((LevelX - Xpos) / delta);
-					} 
-				}
-				Xflag = !Xflag; // Switch flag for next iteration
-	*/		}
+			}
 	
 			else 
 			{
 				LevelFlag = false; //If switch is off reset LevelFlag
+				
+				//Disable motors to allow for manual adjustment
 				motorX.EnableRequest(false);
 				motorY.EnableRequest(false);
 			} 
 		}
-		count += 1;
-		Delay_ms(75);		// Wait a .05 second before the next reading. 50
+		count += 1;			//increase count to take 10 samples
+		Delay_ms(75);		// Wait a .075 second before the next reading.
 	}
 }
  
@@ -208,7 +196,6 @@ void HandleAlertsX();
  * MoveDistanceX
  *
  *    Command "distance" number of step pulses away from the current position
- *    Prints the move status to the USB serial port
  *    Returns when HLFB asserts (indicating the motorX has reached the commanded
  *    position)
  *
@@ -216,99 +203,62 @@ void HandleAlertsX();
  *    int distance  - The distance, in step pulses, to move
  *
  * Returns: True/False depending on whether the move was successfully triggered.
- */
+ -------------------------------------------------------------------------------*/
+
 void MoveDistanceX(int32_t distance) {
     // Check if a motorX alert is currently preventing motion
     // Clear alert if configured to do so 
-     if (motorX.StatusReg().bit.AlertsPresent) {
-    //     SerialPort.SendLine("MotorX alert detected.");       
-    //     PrintAlerts();
-         if(HANDLE_ALERTS){
+     if (motorX.StatusReg().bit.AlertsPresent) 
+	 {
+         if(HANDLE_ALERTS)
+		 {
              HandleAlertsX();
-         } //else {
-    //         SerialPort.SendLine("Enable automatic alert handling by setting HANDLE_ALERTS to 1.");
-    //     }
-    //     SerialPort.SendLine("Move canceled.");      
-    //     SerialPort.SendLine();
-    //     return false;
+         } 
      }
- 
-    // SerialPort.Send("Moving distance: ");
-    // SerialPort.SendLine(distance);
  
     // Command the move of incremental distance
     motorX.Move(distance);
  
     // Waits for HLFB to assert (signaling the move has successfully completed)
-    // SerialPort.SendLine("Moving.. Waiting for HLFB");
     while ( (!motorX.StepsComplete() || motorX.HlfbState() != MotorDriver::HLFB_ASSERTED) &&
             !motorX.StatusReg().bit.AlertsPresent) {
         continue;
     }
-    // // Check if motorX alert occurred during move
-    // // Clear alert if configured to do so 
-    // if (motorX.StatusReg().bit.AlertsPresent) {
-    //     SerialPort.SendLine("MotorX alert detected.");       
-    //     PrintAlerts();
-    //     if(HANDLE_ALERTS){
-    //         HandleAlerts();
-    //     } else {
-    //         SerialPort.SendLine("Enable automatic fault handling by setting HANDLE_ALERTS to 1.");
-    //     }
-    //     SerialPort.SendLine("Motion may not have completed as expected. Proceed with caution.");
-    //     SerialPort.SendLine();
-    //     return false;
-    // } else {
-    //     SerialPort.SendLine("Move Done");
-    //     return true;
-    // }
+   
 }
 
 void MoveDistanceY(int32_t distance) {
     // Check if a motorY alert is currently preventing motion
     // Clear alert if configured to do so 
-     if (motorY.StatusReg().bit.AlertsPresent) {
-    //     SerialPort.SendLine("MotorY alert detected.");       
-    //     PrintAlerts();
-     if(HANDLE_ALERTS){
-		         HandleAlertsY();
-         } 
-    //         SerialPort.SendLine("Enable automatic alert handling by setting HANDLE_ALERTS to 1.");
-    //     }
-    //     SerialPort.SendLine("Move canceled.");      
-    //     SerialPort.SendLine();
-    //     return false;
-     }
- 
-    // SerialPort.Send("Moving distance: ");
-    // SerialPort.SendLine(distance);
+     if (motorY.StatusReg().bit.AlertsPresent) 
+	 {
+		if(HANDLE_ALERTS)
+		{
+			HandleAlertsY();
+        } 
+    }
  
     // Command the move of incremental distance
     motorY.Move(distance);
  
     // Waits for HLFB to assert (signaling the move has successfully completed)
-    // SerialPort.SendLine("Moving.. Waiting for HLFB");
     while ( (!motorY.StepsComplete() || motorY.HlfbState() != MotorDriver::HLFB_ASSERTED) &&
             !motorY.StatusReg().bit.AlertsPresent) {
         continue;
     }
-    /*
-    // Check if motorY alert occurred during move
-    // Clear alert if configured to do so */
-    if (motorY.StatusReg().bit.AlertsPresent) {
-      //  SerialPort.SendLine("MotorY alert detected.");       
-    //    PrintAlerts();
-        if(HANDLE_ALERTS){
-            HandleAlertsY();
-    //    } else {
-    //        SerialPort.SendLine("Enable automatic fault handling by setting HANDLE_ALERTS to 1.");
-        }
-    //    SerialPort.SendLine("Motion may not have completed as expected. Proceed with caution.");
-     //   SerialPort.SendLine();
-     //   return false;
-		}
-    }
+}
 
+/*------------------------------------------------------------------------------
+ * HandleAlerts
+ *
+ *    If a motor alert is present disable motor and wait for alert to clear
+ *	  then re-enable the motor before making adjustment.
+ *
+ * Parameters:
+ *    None
+ *
+ * Returns: Nothing
+ -------------------------------------------------------------------------------*/
 void HandleAlertsX(){
 	if(motorX.AlertReg().bit.MotorFaulted){
 		motorX.EnableRequest(false);
